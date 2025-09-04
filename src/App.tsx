ef69fd9529e9ce355a610fea1bdbaf0164f8190c
seastj <dev.seastj@gmail.com>
2025-09-04 23:56:42 +0900

    [docs] todo 진행중

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
-import React from 'react'
+import React from 'react';
+import TodoList from './components/todos/TodoList';
+import TodoWrite from './components/todos/TodoWrite';
 
 function App() {
   return (
     <div>
       <h2>Todos</h2>
+      <TodoWrite />
+      <TodoList />
       <button>등록</button>
       <button>삭제</button>
-
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
